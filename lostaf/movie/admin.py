8a8699bd0be4104ffc74c73238760722c0da3da7
David Quintero <dquinterg1@eafit.edu.co>
2025-08-21 17:02:51 -0500

    feat(movie): add genre and year fields + improve admin view

--- lostaf/movie/admin.py
+++ lostaf/movie/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from .models import Movie
 
-admin.site.register(Movie)
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+  class MovieAdmin(admin.ModelAdmin):
+    list_display = ('title', 'genre', 'year')
+    search_fields = ('title', 'genre')
+    list_filter = ('genre', 'year')
--- lostaf/movie/models.py
+++ lostaf/movie/models.py
@@ -2,9 +2,13 @@ from django.db import models
 
 class Movie(models.Model):
     title = models.CharField(max_length=100)
-    description = models.TextField()
-    image = models.ImageField(upload_to='movie/images')
-    url = models.URLField(blank=True)
+    description = models.TextField(blank=True, default='')
+    image = models.ImageField(upload_to='movie/images', blank=True, null=True)
+    url = models.URLField(blank=True, null=True)
+
+    # 🔥 Nuevos campos:
+    genre = models.CharField(max_length=50, blank=True, null=True)
+    year = models.IntegerField(blank=True, null=True)
 
     def __str__(self):
         return self.title
